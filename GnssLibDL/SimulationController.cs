97276d05a19bc64abfafe36f9f0ecab438d1b64f
Simon <zomon1@hotmail.com>
2024-03-28 13:21:22 +0100

    updated controller

--- GnssLibDL/SimulationController.cs
+++ GnssLibDL/SimulationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Exjobbv2.Models;
+using Exjobbv2.Models.BroadCastDataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +10,58 @@ namespace GnssLibDL
 {
     internal class SimulationController
     {
-        private bool useGPS { get; set; }
-        private bool useGalileo { get; set; }
-        private bool useGalileo { get; set; }
+        private bool useGPS;
+        private bool useGalileo;
+        private bool useGlonass;
+        private DateTime dt;
+        private double latPos;
+        private double longPos;
+        private bool useJammer;
+        private double jamRad;
+        private double jamLat;
+        private double jamLong;
+        private Satellites GNSS_Data;
 
-        public SimulationController(bool gps, bool galileo, bool glonass, DateTime dateTime, double latPos, double longPos, bool jammer, double jamRad, double jamLat, double jamLong) { 
-            
+        private double updateLat;
+        private double updateLong;
+
+
+
+        public SimulationController(bool gps, bool galileo, bool glonass, DateTime dateTime, double latPos, double longPos, bool jammer, double jamRad, double jamLat, double jamLong) {
+            useGPS = gps;
+            useGalileo = galileo;
+            useGlonass = glonass;
+            dt = dateTime;
+            this.latPos = latPos;
+            this.longPos = longPos;
+            useJammer = jammer;
+            this.jamRad = jamRad;
+            this.jamLat = jamLat;
+            this.jamLong = jamLong;
+
+            string filePath = $"Resources/Broadcast/{dt.Year}_{ dt.Month}_{dt.Day}_BroadCastFile.rnx"; // 2024_01_01_BroadCastFile.rnx
+            //BroadCastDataReader bcdr = new BroadCastDataReader();
+            //GNSS_Data = bcdr.ReadBroadCastData(filePath);
+
+
         }
 
         public void Tick()
         {
+            if(latPos != updateLat) { latPos = updateLat; } 
+            if(longPos != updateLong) { longPos = updateLong; }
+            
 
+
+
+
+
+        }
+
+        public void UpdatePos(double latPos, double longPos)
+        {
+            updateLat = latPos;
+            updateLong = longPos;
         }
 
 
