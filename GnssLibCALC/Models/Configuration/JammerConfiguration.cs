f08bae4011e99a112724a0b3386f06bc284523eb
Simon <zomon1@hotmail.com>
2024-04-05 09:02:58 +0200

    Added Config Models

---
+++ GnssLibCALC/Models/Configuration/JammerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GnssLibCALC.Models.Configuration
+{
+    internal class JammerConfiguration
+    {
+        public bool IsJammerOn { get; set; }
+        public double JammerRadius { get; set; }
+        public double JammerLatitude { get; set; }
+        public double JammerLongitude { get; set; }
+    }
+}
---
+++ GnssLibCALC/Models/Configuration/ReceiverConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GnssLibCALC.Models.Configuration
+{
+    internal class ReceiverConfiguration
+    {
+        public bool IsUsingGPS { get; set; }
+        public bool IsUsingGalileo { get; set; }
+        public bool IsUsingGlonass { get; set; }
+        public DateTime ReceiverDT { get; set; }
+        public double ReceiverLatitude { get; set; }
+        public double ReceiverLongitude { get; set; }
+        public double ReceiverElevetion { get; set; }
+    }
+}
